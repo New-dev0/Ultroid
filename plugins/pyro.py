7c2bb8b8a4e99554327383f522ada9ef1faba994
Anonymous <69723581+New-dev0@users.noreply.github.com>
2021-03-12 12:19:50 +0530

    Update pyro.py

--- plugins/pyro.py
+++ plugins/pyro.py
@@ -1,14 +1,13 @@
 import os
 from pyrogram import Client,filters
 from . import *
-from pytgcalls import PyTgCalls
-
+from pytgcalls import GroupCalls
 import logging
 logging.basicConfig(level=logging.INFO)
 
 PYROSESSION = os.environ.get("PYROSESSION")
 Client = Client(PYROSESSION,api_id=Var.API_ID,api_hash=Var.API_HASH)
-pytgcalls = PyTgCalls(Client)
+pytgcalls = GroupCalls(Client)
 
 @Client.on_message(filters.outgoing & filters.command(["pyro"],"."))
 async def ree(client,message):
@@ -16,8 +15,7 @@ async def ree(client,message):
   
 @Client.on_message(filters.outgoing & filters.command(["join"],"."))
 async def joinno(_,message):
-  await pytgcalls.join_group_call(message.chat.id,None,48000)
+  await pytgcalls.start(message.chat.id)
   await message.edit_text("Joined VC !!")
 
 Client.run()
-pytgcalls.run()
